b525a9e97475397ba76a0ee851fc066541bbfead
iamhagen <jonas.hagenlocher@gmail.com>
2024-12-30 14:56:05 +0100

    Füge Leerzeilen im PostHeader-Komponenten-Layout hinzu

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -13,7 +13,9 @@ type Props = {
 
 export function PostHeader({ title, coverImage, date, author }: Props) {
   return (
+
     <>
+
       <PostTitle>{title}</PostTitle>
       <div className="hidden md:block md:mb-12">
         {/* <Avatar name={author.name} picture={author.picture} /> */}
